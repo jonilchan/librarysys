d614368e89b8372d79a094664ad727bd184ce776
Jonil <1158984345@qq.com>
2021-11-23 21:38:16 +0800

    Initial commit

---
+++ src/main/java/com/gdufe/libsys/CodeGenerator.java
@@ -0,0 +1,55 @@
+package com.gdufe.libsys;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.AbstractTemplateEngine;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CodeGenerator {
+    public static void main(String[] args) {
+        AutoGenerator autoGenerator = new AutoGenerator();
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL);
+        dataSourceConfig.setDriverName("com.mysql.cj.jdbc.Driver");
+        dataSourceConfig.setUsername("root");
+        dataSourceConfig.setPassword("123456");
+        dataSourceConfig.setUrl("jdbc:mysql://localhost:3306/librarysys?useUnicode=true&characterEncoding=UTF-8&serverTimezone=GMT");
+        autoGenerator.setDataSource(dataSourceConfig);
+        GlobalConfig globalConfig = new GlobalConfig();
+        globalConfig.setOpen(true);
+        globalConfig.setOutputDir(System.getProperty("user.dir")+"/src/main/java");
+        globalConfig.setAuthor("jonil");
+        globalConfig.setServiceName("%sService");
+        autoGenerator.setGlobalConfig(globalConfig);
+        PackageConfig packageConfig = new PackageConfig();
+        packageConfig.setParent("com.gdufe.libsys");
+        packageConfig.setEntity("entity");
+        packageConfig.setMapper("mapper");
+        packageConfig.setController("controller");
+        packageConfig.setService("service");
+        packageConfig.setServiceImpl("service.impl");
+        autoGenerator.setPackageInfo(packageConfig);
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig.setEntityLombokModel(true);
+        strategyConfig.setNaming(NamingStrategy.underline_to_camel);
+        strategyConfig.setColumnNaming(NamingStrategy.underline_to_camel);
+        List<TableFill> list = new ArrayList<>();
+        TableFill tableFill1 = new TableFill("create_time",FieldFill.INSERT);
+        TableFill tableFill2 = new TableFill("update_time",FieldFill.INSERT_UPDATE);
+        list.add(tableFill1);
+        list.add(tableFill2);
+
+        strategyConfig.setTableFillList(list);
+        autoGenerator.setStrategy(strategyConfig);
+
+        autoGenerator.execute();
+    }
+}
---
+++ src/test/java/com/gdufe/libsys/LibrarysysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdufe.libsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarysysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
