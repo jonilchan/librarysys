6e442f7e026c513459148e702bdc0e407d01ec5b
Jonil <1158984345@qq.com>
2021-11-24 19:43:21 +0800

    Initial commit

--- src/main/java/com/gdufe/libsys/controller/BaseController.java
+++ src/main/java/com/gdufe/libsys/controller/BaseController.java
@@ -2,6 +2,7 @@ package com.gdufe.libsys.controller;
 
 
 import com.gdufe.libsys.utils.ResultInfo;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,8 +11,8 @@ public class BaseController {
 
 
     @ModelAttribute
-    public void preHandler(HttpServletRequest request){
-        request.setAttribute("ctx", request.getContextPath());
+    public void preHandler(HttpServletRequest request, Model model){
+        request.setAttribute("ctx", request.getContextPath() + "public/");
     }
 
 
